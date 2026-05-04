5c98cdb74a05f500c04a6a903641db08174d0bed
26LP99664639 <26LP99664639@admins-iMac2b-305.local>
2026-05-04 13:32:32 +0800

    java add

---
+++ Javascript/index.js
@@ -0,0 +1,21 @@
+// 1. Өгөгдсөн тоо нь 5-д хуваагддаг үгүйг шалгана уу.
+
+let too = 100;
+let b = 5;
+
+if (too % 5 == 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+// 2. Өгөгдсөн үсгийг шалгаад эгшиг бол "vowel" , гийгүүлэгч бол "consonant" гэж хэвлэнэ үү .
+
+// 3. Өгөгдсөн тоо 1-ээс 100-н хооронд байвал "include" , байхгүй бол "exclude" гэж хэвлэнэ үү .
+
+let a = 101;
+
+if (a >cd = 1 && a < 100) {
+  console.log("include");
+} else {
+  console.log("exclude");
+}
