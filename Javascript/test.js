63ecbcf3eb55679c25e1939adcec462e26b0a75b
Duurnbileg <duurnbileg@gmail.com>
2026-05-04 15:39:47 +0800

    problems

---
+++ Javascript/test.js
@@ -0,0 +1,15 @@
+// 6. Check the given number is not a prime number .
+
+let givenNumber = 11;
+let divider = 1;
+
+if (
+  divider >= 2 &&
+  diveder <= givenNumber &&
+  givenNumber % givenNumber === 0 &&
+  givenNumber % divider === 0
+) {
+  console.log("PrimeNumber");
+} else {
+  console.log("Non Prime");
+}
