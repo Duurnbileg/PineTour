f2a5821b9a1f694cb6dc4c7e2f71e91117ae65c0
26LP99664639 <26LP99664639@admins-iMac2b-305.local>
2026-05-05 17:20:51 +0800

    problem solved

---
+++ Javascript/loop.js
@@ -0,0 +1,84 @@
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i += 3) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + "Tegsh too");
+//   } else {
+//     console.log(i + "Sondgoi too");
+//   }
+// }
+
+// for (let i = 0; i <= 33; i += 3) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 0; i <= 33; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+console.log("1iin hurd");
+for (let i = 1; i <= 1; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("2iin hurd");
+for (let i = 2; i <= 2; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("3iin hurd");
+for (let i = 3; i <= 3; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("4iin hurd");
+for (let i = 4; i <= 4; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("5iin hurd");
+for (let i = 5; i <= 5; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("6iin hurd");
+for (let i = 6; i <= 6; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("7iin hurd");
+for (let i = 7; i <= 7; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("8iin hurd");
+for (let i = 8; i <= 8; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
+console.log("9iin hurd");
+for (let i = 9; i <= 9; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(i, "x", j, "=", i * j);
+  }
+}
--- Javascript/test.js
+++ Javascript/test.js
@@ -1,15 +1,134 @@
-// 6. Check the given number is not a prime number .
+// let i = 4 > 3; // true
+// if ((i = 4 > 3)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let ii = 4 < 3; // false
+// if ((ii = 4 < 3)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let iii = 4 >= 3; //true
+// if ((iii = 4 >= 3)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let iv = 4 <= 3; //false
+// if ((iv = 4 <= 3)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let v = 4 == 4; //true
+// if ((v = 4 == 4)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let vi = 4 === 4; //true
+// if ((vi = 4 === 4)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let vii = 4 != 4; //false
+// if ((vii = 4 != 4)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let iix = 4 !== 4; //false
+// if ((iix = 4 !== 4)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let ix = 4 != "4"; //false
+// if ((ix = 4 != "4")) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let x = 4 == "4"; //true
+// if ((x = 4 == "4")) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let xi = 4 === "4"; //false
+// if ((xi = 4 === "4")) {
+//   console.log("!","true");
+// } else {
+//   console.log("false");
+// }
+
+// let one = 4 > 3 && 10 < 12; //true
+// if ((one = 4 > 3 && 10 < 12)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let two = 4 > 3 && 10 > 12; //false
+// if ((two = 4 > 3 && 10 > 12)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let three = 4 > 3 || 10 < 12; //true
+// if ((three = 4 > 3 || 10 < 12)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let four = 4 > 3 || 10 > 12; //true
+// if ((four = 4 > 3 || 10 > 12)) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let five = !(4 > 3); //false
+// if ((five = !(4 > 3))) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let six = !(4 < 3); //true
+// if ((six = !(4 < 3))) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let eight = !(4 > 3 && 10 < 12); //false
+// if ((eight = !(4 > 3 && 10 < 12))) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let nine = !(4 > 3 && 10 > 12); //true
+// if ((nine = !(4 > 3 && 10 > 12))) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let ten = !(4 === "4"); //true
+// if ((ten = !(4 === "4"))) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
 
 let givenNumber = 11;
 let divider = 1;
 
-if (
-  divider >= 2 &&
-  diveder <= givenNumber &&
-  givenNumber % givenNumber === 0 &&
-  givenNumber % divider === 0
-) {
-  console.log("PrimeNumber");
-} else {
-  console.log("Non Prime");
+for (let givenNumber = 0; givenNumber <= givenNumber; index++) {
+  if (divider <2) {console.log();
+  
+  } else if (givenNumber % givenNumber === 0 && givenNumber % divider === 0) {
+    console.log("PrimeNumber");
+  } else {
+    console.log("Non Prime");
+  }
 }
