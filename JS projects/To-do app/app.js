8f7062d0f1190481e3809017158cccb27ab3553b
Duurnbileg <duurnbileg@gmail.com>
2026-05-20 14:31:25 +0800

    done

--- JS projects/To-do app/app.js
+++ JS projects/To-do app/app.js
@@ -1,22 +1,17 @@
-let newBtn = document.createElement("button")
-
-newBtn.innerText = "Namaig dar!"
-newBtn.style.padding = "10px 20px"
-newBtn.style.backgroundColor = "orange"
-newBtn.style.borderRadius = "5px"
-
 function addTask() {
     let taskListEl = document.getElementById("taskList")
     let inputEl = document.getElementById("taskInput")
 
     let xBtn = document.createElement("button")
-    xBtn.innerText = "Remove"
+    xBtn.innerText = "Done!"
     let li = document.createElement("li")
     li.innerText = inputEl.value
     li.appendChild(xBtn)
     li.onclick = (xBtn) => li.remove()
 
-
+    if (inputEl.value == "") {
+        addTask()
+    }
 
     taskListEl.appendChild(li)
     inputEl.value = ""
