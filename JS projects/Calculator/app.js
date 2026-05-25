75aac89dfa56fa9454716b143c41b44afcddf4b8
Duurnbileg <duurnbileg@gmail.com>
2026-05-25 09:03:06 +0800

    0525

--- JS projects/Calculator/app.js
+++ JS projects/Calculator/app.js
@@ -1,1 +1,43 @@
-impor
+let display = document.querySelector("#input")
+let current = ""
+let hadgalsan = 0
+let temdeg = ""
+
+function clickNumber(param) {
+    current = current + param
+    display.textContent = current
+}
+
+function uildel(param) {
+    hadgalsan = Number(current)
+    current = ""
+    temdeg = param
+}
+
+function equal() {
+    let sum = 0
+    if (temdeg == "+") {
+        sum = hadgalsan + Number(current)
+        display.textContent = sum
+    } else if (temdeg == "*") {
+        sum = hadgalsan * Number(current)
+        display.textContent = sum
+    } else if (temdeg == "-") {
+        sum = hadgalsan - Number(current)
+        display.textContent = sum
+    } else if (temdeg == "/") {
+        sum = hadgalsan / Number(current)
+        display.textContent = sum
+    }
+}
+
+function percent() {
+    
+}
+
+function clearAll() {
+    current = ""
+    hadgalsan = 0
+    temdeg = ""
+    display.textContent = 0
+}
