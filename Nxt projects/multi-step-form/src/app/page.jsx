54f4eaa37063458f8a4bf4b2b3da7ebc902d085f
Duurnbileg <duurnbileg@gmail.com>
2026-06-18 10:04:41 +0800

    multi step added

---
+++ Nxt projects/multi-step-form/src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-[#f4f4f4] flex items-center justify-center h-dvh">
+      <div className="bg-white w-75 h-75 rounded-l"></div>
+    </main>
+  )
+}
