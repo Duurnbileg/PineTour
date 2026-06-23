ecb190046bae3901216ce3ccfb9377e0c015fbc7
26LP99664639 <26LP99664639@admins-iMac2b-305.local>
2026-06-23 11:29:54 +0800

    multi featured added

--- Nxt projects/multi-step-form/src/app/components/input.jsx
+++ Nxt projects/multi-step-form/src/app/components/input.jsx
@@ -1,10 +1,10 @@
-export default function Input({ inputType }) {
+export default function Input({ inputType, placeholder }) {
     return (
         <div className="flex flex-col gap-1">
             <p className="text-black text-sm font-semibold">First name <span className="text-red-500 text-sm">*</span></p>
             <input
                 type={inputType}
-                placeholder="Your first name"
+                placeholder={placeholder}
                 className="flex-1 border border-gray-300 rounded-md px-3 py-2 text-black font-medium"
             />
         </div>
--- Nxt projects/multi-step-form/src/app/page.jsx
+++ Nxt projects/multi-step-form/src/app/page.jsx
@@ -7,6 +7,10 @@ import Step3 from "./features/step3";
 
 export default function Home() {
   const [step, setStep] = useState(1)
+
+  const handleNextStep = () => setStep(step + 1)
+  const handleBackStep = () => setStep(step - 1)
+
   return (
     <main className="bg-[#f4f4f4] flex items-center justify-center h-dvh">
       <Step1 />
